e3302af85070e0e49a88d84b9aab453cc5eb1428
LuCinema <luciferknightingale@gmail.com>
2025-05-22 17:20:14 +1200

    slight edit

--- VIRT-Backend/server.js
+++ VIRT-Backend/server.js
@@ -12,7 +12,7 @@ app.use(express.raw({type: 'application/octet-stream', limit: '20mb'}));
 app.post('/api/predict', async (req, res) => {
     try {
         const url = `https://${process.env.AZURE_REGION}.api.cognitive.microsoft.com/customvision/v3.0/Prediction/${process.env.AZURE_PROJECT_ID}/classify/iterations/${process.env.AZURE_MODEL}/image`;
-        console.log('Azure URL:', url); // 👈 This is what you're adding
+        console.log('Azure URL:', url);
 
         const response = await axios.post(
             url,
